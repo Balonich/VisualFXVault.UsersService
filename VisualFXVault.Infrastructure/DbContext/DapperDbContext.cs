f73b6cc1261951d7ec5df759ea509d07579548ad
Nikita Larushkin <balonich@gmail.com>
2025-04-27 22:10:03 +0200

    Updated Dockerfile. Added connection string to Postgres with env variables

--- VisualFXVault.Infrastructure/DbContext/DapperDbContext.cs
+++ VisualFXVault.Infrastructure/DbContext/DapperDbContext.cs
@@ -12,15 +12,20 @@ public class DapperDbContext
     public DapperDbContext(IConfiguration configuration)
     {
         _configuration = configuration;
-        var connectionString = _configuration.GetConnectionString("PostgresConnection");
 
-        if (string.IsNullOrEmpty(connectionString))
+        var connectionStringTemplate = _configuration.GetConnectionString("PostgresConnection");
+
+        if (string.IsNullOrEmpty(connectionStringTemplate))
         {
             throw new ArgumentNullException("Connection string is not configured in appsettings.json");
         }
 
+        var connectionString = connectionStringTemplate
+            .Replace("$POSTGRES_HOST", Environment.GetEnvironmentVariable("POSTGRES_HOST"))
+            .Replace("$POSTGRES_PASSWORD", Environment.GetEnvironmentVariable("POSTGRES_PASSWORD"));
+
         _connection = new NpgsqlConnection(connectionString);
     }
-    
+
     public IDbConnection DbConnection => _connection;
 }
